3689ccff255b532d9a1b1863d286bd4ded3fe6b6
Dan Tylkowski <dtylkowski@marvel.com>
2015-07-07 16:54:10 -0400

    heading home

--- api.js
+++ api.js
@@ -1,3 +1,23 @@
-var restify = require('restify');
+var restify = require( 'restify' );
 var server = restify.createServer();
-server.use(restify.bodyParser());
+server.use( restify.bodyParser() );
+
+var mongoose = require( 'mongoose/' );
+db = mongoose.connect( process.env.MONGO_URI );
+Schema = mongoose.Schema;
+
+// Create a schema for our data
+var TestSchema = new Schema({
+	date: Date,
+	test: String,
+	working: String
+});
+
+// Use the schema to register a model with MongoDb
+mongoose.model( 'Test', TestSchema );
+var Message = mongoose.model( 'Test' );
+
+function getMessages( req, res, next ) {
+	res.header( 'Access-Control-Allow-Origin', '*' );
+	res.header( 'Access-Control-Allow-Headers', 'X-Requested-With' );
+}
