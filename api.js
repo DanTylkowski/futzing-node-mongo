cae871e24aaa1526ded1960ac79d09642bc33c06
Dan Tylkowski <dtylkowski@marvel.com>
2015-07-08 16:08:02 -0400

    made a simple restify api

--- api.js
+++ api.js
@@ -1,44 +1,134 @@
-var restify = require( 'restify' );
-var server = restify.createServer();
-server.use( restify.bodyParser() );
-
-var mongoose = require( 'mongoose/' );
-db = mongoose.connect( process.env.MONGO_URI );
-Schema = mongoose.Schema;
-
-// Create a schema for our data
-var TestSchema = new Schema({
-	date: Date,
-	test: String,
-	working: String
+var restify = require('restify');
+var mongoose = require('mongoose');
+var server = restify.createServer({
+	name: 'mongo-api'
 });
 
-// Use the schema to register a model with MongoDb
-mongoose.model( 'Test', TestSchema );
-var Message = mongoose.model( 'Test' );
+server.listen( 7000, function() {
+	console.log( '%s listening at %s', server.name, server.url );
+});
 
-function getMessages( req, res, next ) {
-	res.header( 'Access-Control-Allow-Origin', '*' );
-	res.header( 'Access-Control-Allow-Headers', 'X-Requested-With' );
-	Message.find()
-		.sort('date', -1)
-		.execFind(
-			function( arr, data ) {
-				res.send( data );
+var db = mongoose.connect( process.env.MONGO_URI );
+
+var Schema = mongoose.Schema;
+var ObjectID = Schema.ObjectID;
+
+var Thing = new Schema({
+	thing1: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	anotherThing: {
+		type: String,
+		required: false,
+		trim: true
+	},
+	finalThing: {
+		type: String,
+		required: false,
+		trim: true
+	}
+});
+
+var Thing = mongoose.model( 'Thing', Thing );
+
+server
+	.use( restify.fullResponse() )
+	.use( restify.bodyParser() )
+	.use( restify.queryParser() )
+	.use( restify.jsonp() );
+
+server.get(
+	'/thing',
+	function( req, res, next ) {
+		Thing.find(
+			{},
+			function( error, things ) {
+				res.send( things );
 			}
 		);
-}
+	}
+);
 
-function postMessage( req, res, next ) {
-	res.header( 'Access-Control-Allow-Origin', '*' );
-	res.header( 'Access-Control-Allow-Headers', 'X-Requested-With' );
-	var message = new Message();
-	message.message = req.params.message;
-	message.date = new Date();
-	message.save(function() {
-		res.send( req.body );
-	});
-}
+server.post(
+	'/thing',
+	function( req, res, next ) {
+		if( req.params.thing1 === undefined ) {
+			console.log( req.params );
+			console.log( req.params.thing1 );
+			console.log( req.params.anotherThing );
+			console.log( req.params.finalThing );
+			return next( new restify.InvalidArgumentError( 'thing1 must be supplied' ) );
+		}
+		var thingData = {
+			thing1: req.params.thing1,
+			anotherThing: req.params.anotherThing,
+			finalThing: req.params.finalThing
+		};
+		var thing = new Thing( thingData );
+		thing.save(function( error, data ) {
+			if( error ) {
+				return next( new restify.InvalidArgumentError( JSON.stringify( error.errors ) ) );
+			} else {
+				res.json( data );
+			}
+			res.send( 201, thing );
+		});
+	}
+);
 
-server.get( '/messages', getMessages );
-server.post( '/messages', postMessage );
+server.get(
+	'/thing/addthing',
+	function( req, res, next ) {
+		if( req.params.thing1 === undefined ) {
+			console.log( req.params );
+			console.log( req.params.thing1 );
+			console.log( req.params.anotherThing );
+			console.log( req.params.finalThing );
+			return next( new restify.InvalidArgumentError( 'thing1 must be supplied' ) );
+		}
+		var thingData = {
+			thing1: req.params.thing1,
+			anotherThing: req.params.anotherThing,
+			finalThing: req.params.finalThing
+		};
+		var thing = new Thing( thingData );
+		thing.save(function( error, data ) {
+			if( error ) {
+				return next( new restify.InvalidArgumentError( JSON.stringify( error.errors ) ) );
+			} else {
+				res.json( data );
+			}
+			res.send( 201, thing );
+		});
+	}
+);
+
+//curl -i -X POST -d '{ "thing1": "hi", "anotherThing": "hello", "finalThing": "yo" } ' http://localhost:7000/thing
+//curl -i -X POST -d 'thing1=hi&anotherThing=hello&finalThing=yo' http://localhost:7000/thing
+//curl -i -X POST -d 'thing1=hey-ya&anotherThing=yup-yup&finalThing=champ' http://localhost:7000/thing
+
+server.get(
+	'/thing/bything',
+	function( req, res, next ) {
+		console.log( req.params );
+		Thing.find(
+			{
+				thing1: req.params.thing1
+			},
+			function( error, thing ) {
+				if( error ) {
+					return next( new restify.InvalidArgumentError( JSON.stringify( error.errors ) ) );
+				}
+
+				if( thing ) {
+					res.send( thing );
+				} else {
+					res.send( 404 );
+				}
+			}
+		);
+	}
+);
+
