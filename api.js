fe26307fb17fe1762617ce01894912fb4dd9c3cb
Dan Tylkowski <dtylkowski@marvel.com>
2015-07-08 09:29:15 -0400

    picking back up at work

--- api.js
+++ api.js
@@ -20,4 +20,22 @@ var Message = mongoose.model( 'Test' );
 function getMessages( req, res, next ) {
 	res.header( 'Access-Control-Allow-Origin', '*' );
 	res.header( 'Access-Control-Allow-Headers', 'X-Requested-With' );
+	Message.find()
+		.sort('date', -1)
+		.execFind(
+			function( arr, data ) {
+				res.send( data );
+			}
+		);
+}
+
+function postMessage( req, res, next ) {
+	res.header( 'Access-Control-Allow-Origin', '*' );
+	res.header( 'Access-Control-Allow-Headers', 'X-Requested-With' );
+	var message = new Message();
+	message.message = req.params.message;
+	message.date = new Date();
+	message.save(function() {
+		res.send( req.body );
+	});
 }
